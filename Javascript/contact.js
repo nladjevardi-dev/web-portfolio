89ce1860237ecbc5ae9b6d745ca308f4c818e534
nladjevardi-dev <nladjevardi@student.scitech.edu>
2026-09-01 23:15:15 -0700

    Java script troubleshooting

--- Javascript/contact.js
+++ Javascript/contact.js
@@ -1,5 +1,6 @@
 const submitButton = document.getElementById('submit-btn');
 
-submitButton.addEventListener('click', function() {
+submitButton.addEventListener('click', function(event) {
+    event.preventDefault();
     alert('Thank you for your message!');
-})
+});
